93f66bd8258a9f425b869a7664224314ed85cd76
BharathiKannanBabu <bharathikannan130552055@gmail.com>
2024-10-07 10:40:52 +0530

    server endponts added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,31 @@
-import { Route, Routes } from 'react-router-dom'
-import Homepage from './pages/Homepage'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import  SignUpPage  from './pages/SignUpPage'
 import ExplorePage from './pages/ExplorePage'
 import LikesPage from './pages/LikesPage'
 import Sidebar from './components/Sidebar'
 import { Toaster } from 'react-hot-toast'
+import { useAuthContext } from './context/AuthContext.jsx'
+import HomePage from './pages/Homepage'
 
 const App = () => {
+  const {authUser, loading} = useAuthContext();
+  console.log('Authenticated User:', authUser);
+  
+  if(loading) return null;
+
   return (
     <div className='flex text-white'>
       <Sidebar />
       <div className='max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1'>
-        <Routes>
-          <Route path='/' element={<Homepage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/signup' element={<SignUpPage />} />
-          <Route path='/explore' element={<ExplorePage />} />
-          <Route path='/likes' element={<LikesPage />} />
-        </Routes>
-        <Toaster />
+      <Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+					<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
+					<Route path='/explore' element={authUser ? <ExplorePage /> : <Navigate to={"/login"} />} />
+					<Route path='/likes' element={authUser ? <LikesPage /> : <Navigate to={"/login"} />} />
+				</Routes>
+				<Toaster />
       </div>
     </div>
   )
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -14,18 +14,15 @@ const HomePage = () => {
 
 	const [sortType, setSortType] = useState('recent')
 
-	const getUserProfileAndRepos = useCallback(async(username = "BharathiKannanBabu") => {
+	const getUserProfileAndRepos = useCallback(async(username = "BharathikannanBabu") => {
 		setLoading(true)
 		try {
-			const res = await fetch(`http://localhost:5000/api/users/profile/${username}`);
+			const res = await fetch(`/api/users/profile/${username}`);
 			const {repos, userProfile} = await res.json()
 			
 			repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); //descending, recent first
 			setRepos(repos)
 			setUserProdfile(userProfile)
-			
-			console.log('userProfile', userProfile);
-			console.log('Repos', repos);
 		} catch (error) {
 			toast.error(error.message)
 		} finally {
