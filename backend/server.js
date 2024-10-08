5cc0aa49576e079f052b4af9ac36324d8ad1d42d
BharathiKannanBabu <bharathikannan130552055@gmail.com>
2024-10-08 10:03:23 +0530

    optimized for build

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import passport from "passport";
 import session from "express-session";
+import  path  from "path";
 
 import './passport/github.auth.js'
 
@@ -14,6 +15,10 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+console.log(__dirname);
 
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 // Initialize Passport!  Also use passport.session() middleware, to support
@@ -22,15 +27,17 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send("Server is ready");
-})
-
 app.use('/api/auth', authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/explore", exploreRoutes)
 
-app.listen(5000, () => {
-    console.log("Server started");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (res, req) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running to the port: ${PORT}`);
     connectDatabase()
 })
